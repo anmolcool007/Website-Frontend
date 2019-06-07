abec1a33d8e45652c5d0b2b2c5e5d387de0137db
anmolcool007 <43794036+anmolcool007@users.noreply.github.com>
2019-06-07 16:10:30 +0530

    Add files via upload

---
+++ COMMON_TO_ALL/js/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+	var offset = 5,
+		offset_opacity = 5,
+		scroll_top_duration = 800,
+		$back_to_top = $('.cd-top');
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+});
